34df8122fb13b09de167a6a02963a376f70097e6
farewell12345 <1149558764@qq.com>
2024-05-12 23:16:19 +0800

    fix: 缩短短连接

--- config/conf.go
+++ config/conf.go
@@ -5,6 +5,6 @@ const (
 	Mode        = "debug"
 	ProjectName = "Kuroko"
 	RedisHost   = "101.42.18.21:6379"
-	Domain      = "https://umb.ink/kuroko/tinyurl/"
+	Domain      = "https://umb.ink/kuroko/"
 	//Domain = "http://127.0.0.1:8080/tinyurl/"
 )
