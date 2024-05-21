e5c65720bfcb13478fe414aa0cd3fa2e7d911d75
farewell12345 <1149558764@qq.com>
2024-05-21 22:02:24 +0800

    fix: 删除docker-compose的相关redis依赖

--- config/conf.go
+++ config/conf.go
@@ -1,6 +1,7 @@
 package conf
 
 import (
+	"kuroko/internal/pkg/util"
 	"log"
 	"os"
 )
@@ -9,15 +10,16 @@ var (
 	Port        = "8080"
 	Mode        = "debug"
 	ProjectName = "Kuroko"
-	RedisHost   = "101.42.18.21:6379"
-	RedisPwd    = "umbink"
-	Domain      = "https://umb.ink/k/"
+	RedisHost   = ""
+	RedisPwd    = ""
+	Domain      = ""
 	//Domain = "http://127.0.0.1:8080/tinyurl/"
 )
 
 func LoadConfig() {
-	Domain = os.Getenv("DOMAIN")
 	Port = os.Getenv("PORT")
+	Domain = "http://" + util.GetIP() + ":" + Port + "/k/"
+	Domain = os.Getenv("DOMAIN")
 	RedisHost = os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT")
 	RedisPwd = os.Getenv("REDIS_PWD")
 	log.Println("load config", Domain, Port, RedisHost, RedisPwd)
